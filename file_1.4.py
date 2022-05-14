9ded971a05ef7a98594842aac68df983336f784e
mightykatun <91433828+mightykatun@users.noreply.github.com>
2022-05-14 21:45:26 +0200

    Add files via upload

---
+++ file_1.4.py
@@ -0,0 +1,29 @@
+print("file modifier")
+filename = input("filepath: ")
+file = open(filename)
+text = file.read()
+print("content:")
+print(text)
+while True:
+      choice = input("modify y/n: ")
+      if choice == "n":
+            file.close()
+            print("file closed")
+            break
+
+      elif choice == "y":
+            file.close()
+            file1 = open(filename, 'w')
+            file1.write(input("new text: "))
+            file1.close()
+            print("file modified")
+            file2 = open(filename)
+            text1 = file2.read()
+            print("check:")
+            print(text1)
+            file2.close()
+            print("file closed")
+            break
+      else:
+            print("input exception")
+end = input("enter to terminate")
